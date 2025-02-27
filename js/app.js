90dbd8969e1925047f6d56e7256d7ea16b31f3c5
MH-Kniloy <web.kniloy@gmail.com>
2025-02-28 02:39:11 +0600

    image changes on clicking color btn

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const colorBtn = document.getElementsByClassName("ring-button")
+const productImg = document.getElementById("product-image");
 
 for(let i = 0; i < colorBtn.length; i++){
    const singleColorBtn = colorBtn[i]
+     const color = singleColorBtn.id.replace("-color", "")
      
    singleColorBtn.addEventListener("click", function (e){
     for(let j = 0; j < colorBtn.length; j++){
@@ -11,6 +13,6 @@ for(let i = 0; i < colorBtn.length; i++){
     }
     e.target.classList.remove("border-gray-300")
     e.target.classList.add("border-purple-600")
-    
+    productImg.src = `./images/${color}.png`;
    })
 }
